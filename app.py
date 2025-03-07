d3db9f62c7b3cb25d5901edc9762356fa8960757
Dean Meredith <deanjohnmeredith@gmail.com>
2025-03-07 14:50:20 +0000

    updated message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return "Hello, world! What a lovely day it is!"
 
 if __name__ == '__main__':
     app.run(
